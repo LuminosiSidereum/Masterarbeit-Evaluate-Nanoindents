22e04115ac73cbaddb7cf6274de4dd499c0c3f55
Schulti <phillipp-schultheiss@web.de>
2024-10-17 19:33:05 +0200

    Adjsutements in the codebase to get the wanted outputs

--- auswertung_haerte.py
+++ auswertung_haerte.py
@@ -1,34 +1,35 @@
 import pandas as pd #type: ignore
-from pandas import DataFrame #type: ignore
+from pandas import DataFrame,Series #type: ignore
 
-df:DataFrame = pd.read_csv("Ergebnisssammlung_Härte.csv")
+df:DataFrame = pd.read_csv("Ergebnissammlung_Härte.csv")
 
 parametersets:list[str] = ["2000","2100","2200","4000","2001", "2101", "2201", "4001"]
 
 df_sample_results:DataFrame = pd.DataFrame(columns=["Sample","Mean","negYerr","posYerr","Min", "Max"])
-
-for colum in df.columns():
+for colum in df.columns:
+    if df[colum].isnull().all():
+        continue
     mean = df[colum].mean()
-    max = df[colum].max()
-    min = df[colum].min()
-    posYerr = max - mean
-    negYerr = mean - min
-    storage_series = pd.Series([colum, mean, negYerr, posYerr, min, max], index = df_sample_results.columns)
-    df_sample_results = pd.concat([df_sample_results, storage_series], axis=1)
+    max_value = df[colum].max()
+    min_value = df[colum].min()
+    posYerr = max_value - mean
+    negYerr = mean - min_value
+    storage_series = pd.Series([colum, mean, negYerr, posYerr, min_value, max_value], index = df_sample_results.columns)
+    df_sample_results = pd.concat([df_sample_results, storage_series.to_frame().T])
     
-df_sample_results.to_csv("Ergebnisssammlung_Härte_Auswertung_Proben.csv", index=False)
+df_sample_results.to_csv("Ergebnissammlung_Härte_Auswertung_Proben.csv", index=False)
 
-df_parameterset_results:DataFrame = pd.DataFrame(columns=["Parameterset","Mean","StDeviation","Min", "Max"])
+df_parameterset_results:DataFrame = pd.DataFrame(columns=["Index","Parameterset","Mean","StDeviation","Min", "Max"]).set_index("Index")
 
 for parameterset in parametersets:
-    df_filter = df.filter(like=parameterset, axis=1)
-    if df_filter.empty:
+    filtered_data :Series = df.filter(like=parameterset, axis=1).stack().reset_index(drop=True)
+    if filtered_data.empty:
         continue
-    mean = df_filter.mean()
-    stdev = df_filter.std()
-    max = df_filter.max()
-    min = df_filter.min()
-    storage_series = pd.Series([parameterset, mean, stdev, min, max], index = df_parameterset_results.columns)
-    df_parameterset_results = pd.concat([df_parameterset_results, storage_series], axis=1)    
+    mean = filtered_data.mean()
+    stdev = filtered_data.std()
+    max_value = filtered_data.max()
+    min_value = filtered_data.min()
+    storage_series = pd.Series([parameterset, mean, stdev, min_value, max_value], index = df_parameterset_results.columns)
+    df_parameterset_results = pd.concat([df_parameterset_results, storage_series.to_frame().T])    
 
-df_parameterset_results.to_csv("Ergebnisssammlung_Härte_Auswertung_Parameterset.csv", index=False)
+df_parameterset_results.to_csv("Ergebnissammlung_Härte_Auswertung_Parameterset.csv", index=False)
